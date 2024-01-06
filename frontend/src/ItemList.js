9f42bedfa8fcd8d616d55d4a9221a0a1063feb71
Belal Kourkmas <BelalKourkmas@gmail.com>
2024-01-06 14:31:21 -0800

    Improve UI

--- frontend/src/ItemList.js
+++ frontend/src/ItemList.js
@@ -5,7 +5,7 @@ const ItemList = ({ items, onDeleteItem, result }) => {
         <ul className="flex mt-3 flex-col gap-2">
             {items.map((item, index) => {
                 const isHighlighted = result && result.selectedItems.some(selectedItem => selectedItem.name === item.name);
-                const itemClasses = `list-group-item ${isHighlighted ? 'border-4 border-green-900 bg-green-700' : 'border-4 border-slate-900 bg-gray-700'} text-white flex justify-between items-center rounded p-2`;
+                const itemClasses = `list-group-item ${isHighlighted ? 'border-4 border-green-900 bg-green-700 ' : 'border-4 opacity-70 border-slate-900 bg-gray-700'} text-white flex justify-between items-center rounded p-2`;
 
                 return (
                     <li key={index} className={itemClasses}>
@@ -14,7 +14,7 @@ const ItemList = ({ items, onDeleteItem, result }) => {
                             className="bg-red-500 text-white py-1 px-2 rounded text-xs transform transition duration-150 ease-in-out active:scale-75"
                             onClick={() => onDeleteItem(index)}
                         >
-                            x
+                            ✖
                         </button>
                     </li>
                 );
--- frontend/src/TutorialModal.js
+++ frontend/src/TutorialModal.js
@@ -1,11 +1,18 @@
 function TutorialModal({ isVisible, onClose, gifUrl }) {
-    if (!isVisible) return null;
-  
-    return (
-      <div onClick={onClose} style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'rgba(0, 0, 0, 0.5)', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <img src={gifUrl} alt="Tutorial" style={{ maxWidth: '90%', maxHeight: '90%' }} />
+  if (!isVisible) return null;
+
+  return (
+      <div 
+          onClick={onClose} 
+          className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-40"
+      >
+          <img 
+              src={gifUrl} 
+              alt="Tutorial" 
+              className="max-w-90p max-h-90p"
+          />
       </div>
-    );
-  }
-  
-  export default TutorialModal
+  );
+}
+
+export default TutorialModal;
